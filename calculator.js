801706d41500081ec3c379dcddb3e130ffb91267
Rohan656 <183319@pakaims.edu.pk>
2023-03-02 17:06:23 +0500

    addition file added

--- calculator.js
+++ calculator.js
@@ -1,15 +1,5 @@
-// Addiing
-function sum(num1, num2) {
+function add(num1, num2) {
+    
     return num1 + num2;
   }
-console.log(sum(2, 3));
-// multiply
-function multiply(num3, num4) {
-    return num3 * num4;
-  }
-  console.log(multiply(2, 3));
-// Divide
-function divide(num5, num6) {
-    return num5 / num6;
-  }
-  console.log(divide(4,5))
+console.log(add(2, 3));
